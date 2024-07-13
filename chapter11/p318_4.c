5ec2c29aaa4fb26ffa9f1fe266133650e9f4f9e9
wl-eeee <129074330+wl-eeee@users.noreply.github.com>
2024-07-13 16:39:03 +0800

    Create p318_4.c

---
+++ chapter11/p318_4.c
@@ -0,0 +1,39 @@
+#include "head.h"
+
+void input1(int n);// n is the maximum number for b
+int main()
+{
+	input1(5);
+
+	return 0;
+}
+
+
+void input1(int n)
+{
+	FILE* fp = fopen("E:\\c\\outputtest\\abc.txt", "r");
+
+	char a[51];
+	char b[51];
+	int na = 0;//counter
+	int nb = 0;//counter
+
+
+	fgets(a, 51, fp);
+
+	while ((a[na] == ' ') || (a[na] == '\n') || (a[na] == '\t'))
+		na++;
+	while ((a[na] != ' ') && (a[na] != '\n') && (a[na] != '\t')&& (nb <n))
+	{
+		b[nb] = a[na];
+		na++;
+		nb++;
+	}
+
+	b[nb] = '\0';//not nb + 1
+	printf("%s\n", b);
+
+	fclose(fp);
+
+
+}
