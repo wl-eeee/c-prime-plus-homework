8521644eef8970aac0b07b2f01337ef2232380f4
wl-eeee <129074330+wl-eeee@users.noreply.github.com>
2024-07-13 08:10:34 +0800

    Create 318_1.c

---
+++ chapter11/318_1.c
@@ -0,0 +1,30 @@
+#include "head.h"
+
+void get1(char* copy, int n);
+
+int main() {
+	char copy[50];
+	get1(copy, 50);
+	printf("%s", copy);
+
+	return 0;
+
+}
+
+void get1(char* copy, int n)
+{
+	FILE* fp = fopen("E:\\c\\outputtest\\abc.txt", "r");
+	char a;
+	int counter = 0;//counter
+	
+	while (((a = getc(fp)) != EOF) && counter < n - 1)
+	{
+		copy[counter] = a;
+		counter++;
+	}
+
+	copy[counter] = '\0'; // 添加字符串结束符
+	fclose(fp);
+
+
+}
