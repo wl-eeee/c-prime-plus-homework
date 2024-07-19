530af0ab62d3ba3103ae833f7a23c949ff8c331e
wl-eeee <129074330+wl-eeee@users.noreply.github.com>
2024-07-19 20:37:20 +0800

    Update p237_11.c

--- chapter9/p237_11.c
+++ chapter9/p237_11.c
@@ -1,18 +1,48 @@
 #include "head.h"
+
+int fib(int n);
+int fib2(int n);
 int main()
 {
-	int a1 = 1;
-	int a2 = 1;
-	int n = 3;
-	int all;
-	for (n = 3; n < 46; n++)
-	{
-		all = a1 + a2;
-		printf("%d %d\n", n, all);
-		a1 = a2;
-		a2 = all;
-	}
+	int x = fib2(46);
+	printf("%d", x);
 
 	return 0;
-	
+}
+
+int fib(int n)
+{
+	int all;
+	if (n == 1 || n == 2)
+		all = 1;
+	else
+	{
+		all = fib(n - 1) + fib(n - 2);
+	}
+
+	return all;
+
+}
+
+int fib2(int n)
+{
+	int x = 3;//count
+	int all = 0;
+	int a1 = 1;// first item
+	int a2 = 1;// second item
+	if (n == 1 || n == 2)
+		all = 1;
+	else
+	{
+		for (; x <= n; x++)
+		{
+			all = a1 + a2;
+			a1 = a2;
+			a2 = all;
+			printf("%d\n", all);
+		}
+
+	}
+
+	return all;
 }
