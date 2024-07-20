166d3f73cbcdc164bdcaa595526d18b694826c0b
wl-eeee <129074330+wl-eeee@users.noreply.github.com>
2024-07-20 08:12:00 +0800

    Create p273_1.c

---
+++ chapter10/p273_1.c
@@ -0,0 +1,23 @@
+#include "head.h"
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int data[5][5];
+	for (a= 0; a < 5; a++)
+	{
+		for (b = 0; b < 5; b++)
+			data[a][b] = a * b;
+	}
+	int all = 0;
+	for (b = 0; b < 5; b++)
+	{
+		all += data[2][b];
+	}
+
+	printf("%d\n", all / 5);
+		
+	
+
+	return 0;
+}
